8d1ea16db42e93d9d784705ffe97211d1688e39f
TimurMirzabekov <93447562+TimurMirzabekov@users.noreply.github.com>
2022-10-28 17:08:59 +0300

    Create Internet_Outlet_Timur.py

---
+++ Internet_Outlet_Timur.py
@@ -0,0 +1,49 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Shop Timur</title>
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+h1, h3{
+font-family: 'Pacifico', cursive;
+}
+ body{
+min-height: 100vh;
+ display: flex ;
+ justify-content: center;
+ background-color: #faff00;
+ color: #000000;
+ align-items: center;
+
+ }
+ article{
+ margin: 0% auto;
+ }   
+</style>
+</head>
+<body>
+<article>
+    <h1>Internet Outlet Timur</h1>
+       <p>Wearning<br>
+        <br>Sorry, at the moment our store</p>
+        <br>is not working on the territory</p>
+        <br>of the Russian Federation </p>
+        <br>because of the special operation</p>
+        <br>We apologize</p>
+        <br>             
+        <br>Delivery</p>
+        <br>delivery throughout</p>
+        <br>Moscow will be delivered</p>
+        <br>in 15 minutes</p>
+        <br>Other</p>
+        <br>Food Shop Timur</p>
+        <br>Bank Timur</p>
+        <br>2022</p>
+        </p>
+    <h3> </h3>
+</article>
+</body>
+</html>
